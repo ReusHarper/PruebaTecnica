acd7ebe2f424a076d82d65e5888a44ceca1d3a4f
ReusHarper <reusharper@comunidad.unam.mx>
2024-01-17 22:52:05 -0600

    Implementacion de enrutamiento centralizado en main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+/* eslint-disable react-refresh/only-export-components */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import { Users, UsersDashboard } from './sections';
+import Sidebar from './sections/sidebar/Sidebar';
+import './reset.css' ;
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+
+const AppLayout = () => {
+    return (
+        <>
+            <Sidebar />
+            <Outlet />
+        </>
+    );
+}
+
+const router = createBrowserRouter([
+    { 
+        element : <AppLayout />,
+        children : [
+            { 
+                path    : '/',
+                element : <Users />
+            },
+            { 
+                path    : '/users-dashboard',
+                element : <UsersDashboard />
+            },
+            { 
+                path    : '/user-edit/:id',
+                element : <Users />
+            },
+        ]
+    }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <RouterProvider router = { router }/>
+    </React.StrictMode>,
+);
